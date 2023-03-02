0363dd0f3440b29e0d35880e7ba3ee66067b7a20
[epserradresi] <epserradresi@gmail.com>
2023-03-02 19:15:15 +0300

    Home page designed.

--- src/Pages/ContentPages/Home/Home.js
+++ src/Pages/ContentPages/Home/Home.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Button from '../../../components/Button/Button';
+
+import styles from './Home.style';
 
 const Home = () => {
   return (
-    <View>
-      <Text>HOME</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.header_title}>HOME</Text>
+        <Icon name="logout" size={30} color="#eceff1" />
+      </View>
+      <View style={styles.info_container}>
+        <View style={styles.logo_container}>
+          <Image source={require('../../../assests/images/triviaLogo.png')} />
+        </View>
+        <View style={styles.user_info_container}>
+          <Image
+            style={styles.user_image}
+            source={require('../../../assests/images/defaultUserImage.jpg')}
+          />
+          <Text style={styles.username}>username</Text>
+        </View>
+        <Text style={styles.ready_text}>Are you ready for the challenge?</Text>
+        <View style={styles.button_container}>
+          <Button text="Start Challenge" theme="secondary" />
+        </View>
+        <Button text="Leaderboard" theme="primary" />
+      </View>
     </View>
   );
 };
---
+++ src/Pages/ContentPages/Home/Home.style.js
@@ -0,0 +1,66 @@
+import {StyleSheet, Dimensions} from 'react-native';
+import colors from '../../../styles/colors';
+
+const deviceSize = Dimensions.get('window');
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.darkblue,
+  },
+  header: {
+    padding: 15,
+    backgroundColor: colors.darkblue,
+    borderBottomWidth: 2,
+    borderColor: colors.orange,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  header_title: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  info_container: {
+    alignItems: 'center',
+  },
+  logo_container: {
+    alignItems: 'center',
+    marginTop: -100,
+  },
+  user_info_container: {
+    backgroundColor: '#e0e0e0',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    marginTop: -100,
+    width: deviceSize.width / 2,
+    borderRadius: 7,
+  },
+  user_image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 20,
+    borderWidth: 1,
+    borderColor: colors.darkblue,
+  },
+  username: {
+    marginRight: 10,
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: colors.darkblue,
+  },
+  ready_text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 50,
+  },
+  button_container: {
+    marginTop: 30,
+    marginBottom: 10,
+  },
+});
